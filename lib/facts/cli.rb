f90f0fc2917caf7445ec83d76020aa05285a26cb
Kelly Kashuda <klkashuda@crimson.ua.edu>
2017-03-30 00:53:02 +0000

    added case statement to let user pick 1-13

--- lib/facts/cli.rb
+++ lib/facts/cli.rb
@@ -8,6 +8,7 @@ class Facts::CLI
     puts
 
     self.menu
+    self.get_input
   end
 
   def menu
@@ -27,11 +28,25 @@ class Facts::CLI
     puts
     puts "Go ahead and enter 1-13, and a random fact will be generated specifc to the category!"
     puts
-
-    self.get_input
   end
 
-  def self.get_input
-  end 
+  def get_input
+    input = gets.strip
+    case input
+    when "1"
+    when "2"
+    when "3"
+    when "4"
+    when "5"
+    when "6"
+    when "7"
+    when "8"
+    when "9"
+    when "10"
+    when "11"
+    when "12"
+    when "13"
+    end
+  end
 
 end
